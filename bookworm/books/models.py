e28ca8444489e3a4fa6e1288739731cfb17d3dbe
Steve Obaga <obaga@Steves-MacBook-Pro.local>
2019-03-18 20:24:33 +0300

    [Finishes #164679136] Adds the book model

--- bookworm/books/models.py
+++ bookworm/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class books(models.Model):
+    title = models.CharField(max_length=200, blank=True, default='')
+    synopsis = models.CharField(max_length=300, blank=True, default='')
+    category = models.CharField(max_length=300, blank=True, default='')
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('title', )
