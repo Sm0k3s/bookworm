5c1811bbc46f97c86c1a9d3c0f102e1ec312d6dd
Steve Obaga <obaga4@gmail.com>
2019-03-19 19:21:01 +0300

    [Finishes #164679140] Adds feature to update book details

--- bookworm/books/urls.py
+++ bookworm/books/urls.py
@@ -5,4 +5,7 @@ from books import views
 urlpatterns = [
     url(r'^books/$', views.BookList.as_view(),
     name=views.BookList.name),
+    url(r'^games/(?P<pk>[0-9]+)/$',
+        views.BookDetail.as_view(),
+        name=views.BookDetail.name),
 ]
--- bookworm/books/views.py
+++ bookworm/books/views.py
@@ -10,3 +10,9 @@ class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     name = 'book-list'
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    name = 'book-detail'
